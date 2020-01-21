7e74ce58b59704d521fcfc5e52623787e34fc25d
cholman <holman.cody@gmail.com>
2020-01-20 23:56:50 -0600

    it's working

--- src/App.js
+++ src/App.js
@@ -25,19 +25,19 @@ function App() {
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
 				<CartContext.Provider value={{cart}}>
-			<Navigation cart={cart} />
+					<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact path="/" 
-				component={Products}
-				/>
+					{/* Routes */}
+					<Route
+						exact path="/" 
+						component={Products}
+						/>
 
-			<Route
-				path="/cart"
-				component={ShoppingCart}
-			/>
-			</CartContext.Provider>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
